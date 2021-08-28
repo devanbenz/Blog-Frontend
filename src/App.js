67b10adf5e43fa05c506d63221a713688fde6957
devanbenz <devandbenz@gmail.com>
2021-08-27 20:32:14 -0500

    update

--- src/App.js
+++ src/App.js
@@ -4,19 +4,46 @@ const blogService = require('./services/blogService')
 
 const App = () => {
     const [blogs, setBlogs] = useState([])
-    
-    useEffect(() => {
-        (async () => {
-            const blog = await blogService.getAll()
-            setBlogs(blog)
-        })()
-    },[])
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [user, setUser] = useState(null)
 
-    console.log(blogs)
+    // useEffect(() => {
+    //     (async () => {
+    //         const blog = await blogService.getAll()
+    //         setBlogs(blog)
+    //     })()
+    // },[])
+
+    const handleLogin = async (e) => {
+        e.preventDefault()
+        const userLogin = blogService.loginService(
+            username,
+            password
+        )
+        console.log(userLogin)
+        setUsername('')
+        setPassword('')
+    }
+
+    const handleUsername = (e) => {
+        setUsername(e.target.value)
+    }
+
+    const handlePassword = e => {
+        setPassword(e.target.value)
+    }
 
     return (
         <div>
-            <h1>Check console</h1>
+            <h1>login</h1>
+            <div>
+                <form onSubmit={handleLogin}>
+                    username <input type="username" value={username} onChange={handleUsername} />
+                    password <input type="password" value={password} onChange={handlePassword} />
+                    <button type="submit">Login</button>
+                </form>
+            </div>
             <Blogs blogs='Test' />
         </div>
     )
--- src/services/blogService.js
+++ src/services/blogService.js
@@ -2,13 +2,24 @@
 
 //define api endpoints
 const blogs = `/api/blogs`
+const login = `/api/login`
 
 const getAll = async () => {
     const req = await fetch(blogs)
     return req.json()
 }
 
+const loginService = async credentials => {
+    try{
+        const req = await fetch(login, {method:"POST", body: credentials})
+        return req.json()
+    }catch(e){
+        console.log(e)
+    }
+}
+
 
 module.exports = {
-    getAll
+    getAll,
+    loginService
 }
